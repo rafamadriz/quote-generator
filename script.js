e27a30579f097d6783127f71433a60e8538019a2
Rafael Madriz <rafa@rafaelmadriz.com>
2020-11-23 17:54:46 -0300

    Completed

---
+++ script.js
@@ -0,0 +1,60 @@
+const quoteContainer = document.querySelector("#quote-container");
+const quoteText = document.querySelector("#quote");
+const authorText = document.querySelector("#author");
+const twitterBtn = document.querySelector("#twitter");
+const newQuoteBtn = document.querySelector("#new-quote");
+const loader = document.querySelector("#loader");
+
+// Show Loading
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+//Get Quote From API
+async function getQuote() {
+  showLoadingSpinner();
+  const proxyUrl = "https://whispering-tor-04671.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    //If Author is blank add 'Unknown'
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
+    } else authorText.innerText = data.quoteAuthor;
+    //Reduce font size for long quotes
+    if (data.quoteText.length >= 120) {
+      quoteText.classList.add("long-quote");
+    } else quoteText.classList.remove("long-quote");
+    quoteText.innerText = data.quoteText;
+    // Stop Loader, Show Quote
+    removeLoadingSpinner();
+  } catch (error) {
+    getQuote();
+  }
+}
+
+// Tweet quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+//On Load
+getQuote();
